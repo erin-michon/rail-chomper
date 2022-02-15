14018240d07e6dcc5d82e616313d37eb45e36509
Erin M <erin.michon@gmail.com>
2022-02-15 15:19:43 -0500

    updated readme and final bugs

--- README.md
+++ README.md
@@ -1,2 +1,26 @@
-# rail-chomper
-NoSQL: A Social Network API
+## Rail-Chomper: A Social Network API
+
+## Description:
+This is an API for a social netword application where uses car share their thoughts, react to friends' thoughts and create a friends list.  The goal was to create an API that uses a NoSQL database so the website can handle large amounts of unstructured data.
+
+* Express.js was utilized to start the server and for routing:
+* MongoDB was utilized as the database, along with Mongoose as an ODM (Object Document Mapper)
+
+## Screenshots/Walkthrough Video:
+
+* View Walkthrough Video [Here](https://watch.screencastify.com/v/PKDVjeiDahlEs1XFrSbC) 
+
+## Installation:
+Installation requirements include:
+* Clone repository
+* npm install
+* npm start
+
+## Contributing:
+Please refer to [Contributor Covenant](https://www.contributor-covenant.org/) for guidelines.
+
+## Questions:
+Please do not hesitate to reach out to me directly with additional questions via my email address or Github Profile below:
+  
+* Email: [erin.michon@gmail.com](mailto:erin.michon@gmail.com) 
+* Github Profile: [erin-michon](https://github.com/erin-michon) 
--- routes/api/thought-routes.js
+++ routes/api/thought-routes.js
@@ -1,25 +1,24 @@
 const router = require('express').Router();
 
 const {
-    getAllThought,
-    getThoughtById,
-    createThought,
-    updateThought,
-    deleteThought,
-    createReaction,
-    deleteReaction
+  getAllThought,
+  getThoughtById,
+  createThought,
+  updateThought,
+  deleteThought,
+  createReaction,
+  deleteReaction
 } = require('../../controllers/thought-controller');
 
 //set up GET all at /api/thoughts
 router
   .route('/')
   .get(getAllThought)
-  .post(createThought);
 
 // set up POST for thoughts /api/thoughts/:userId
 router
-.route('/:userId')
-.post(createThought)
+  .route('/:userId')
+  .post(createThought)
 
 //set up Get one, PUT and DELETE at /api/thoughts/:id
 router
@@ -30,12 +29,12 @@ router
 
 //set up POST at /api/thoughts/:thoughtId/reactions to create a reaction
 router 
-    .route('/:thoughtId/reactions')
-    .post(createReaction)
+  .route('/:thoughtId/reactions')
+  .post(createReaction)
     
 //set up DELETE at /api/:thoughtId/reactions/:reactionId to delete a reaction
 router
-    .route('/:thoughtId/reactions/:reactionId')
-    .delete(deleteReaction);
+  .route('/:thoughtId/reactions/:reactionId')
+  .delete(deleteReaction);
 
 module.exports = router;
